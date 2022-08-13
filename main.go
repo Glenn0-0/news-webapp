44392a099fa59a8b1e4f07e0025b0018deb751fb
Glenn <vlasenkogalia.personal@gmail.com>
2022-08-13 13:09:59 +0300

    improved formatting; removed lang filter

--- main.go
+++ main.go
@@ -24,19 +24,41 @@ type Search struct {
 	Results    *news.Results
 }
 
+// determines if the last page was reached.
+func (s *Search) IsLastPage() bool {
+	return s.NextPage >= s.TotalPages
+}
+
+// returns previous page number.
+func (s *Search) PreviousPage() int {
+	return s.CurrentPage() - 1
+}
+
+// returns current page number.
+func (s *Search) CurrentPage() int {
+	if s.NextPage == 1 {
+		return s.NextPage
+	}
+
+	return s.NextPage - 1
+}
+
+// temlate to parse and validate .html file.
 var tpl = template.Must(template.ParseFiles("index.html"))
 
-
 func main() {
+	// handling errors opening .env file.
 	if err := godotenv.Load(); err != nil {
 		log.Println("Error loading .env file")
 	}
 
+	// getting PORT from .env.
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 
+	// getting API key from .env.
 	apiKey := os.Getenv("NEWS_API_KEY")
 	if apiKey == "" {
 		log.Fatal("Env: API key must be set")
@@ -45,10 +67,11 @@ func main() {
 	myClient := &http.Client{Timeout: 10 * time.Second}
 	newsapi := news.NewClient(myClient, apiKey, 20)
 
+	// initiallize a file server object by passing the directory with all static files.
 	fs := http.FileServer(http.Dir("assets"))
 
 	mux := http.NewServeMux()
-	mux.Handle("/assets/", http.StripPrefix("/assets", fs))
+	mux.Handle("/assets/", http.StripPrefix("/assets", fs)) // connecting CSS files from assets.
 	mux.HandleFunc("/search", searchHandler(newsapi))
 	mux.HandleFunc("/", indexHandler)
 
@@ -57,6 +80,7 @@ func main() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// executing template: checking for errors and being written to ResponseWriter.
 	buf := &bytes.Buffer{}
 	err := tpl.Execute(buf, nil)
 	if err != nil {
@@ -69,35 +93,39 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 func searchHandler(newsapi *news.Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// parsing raw URL into u (URL structure).
 		u, err := url.Parse(r.URL.String())
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
+		// getting parameters from URL structure
 		parameters := u.Query()
 		searchQuery := parameters.Get("q")
-		if searchQuery == "" {
-			searchQuery = "Bitcoin"
-		}
 
+		// getting page number from Query field. Setting page to 1 if not specified.
 		page := parameters.Get("page")
 		if page == "" {
 			page = "1"
 		}
 
+		// fetching results; handling errors.
 		results, err := newsapi.FetchEverything(searchQuery, page)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
+		// converting and assigning page (string) to nextPage (int).
 		nextPage, err := strconv.Atoi(page)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
+		// declaring the Search struct.
+		// computing total pages by dividing number of results by the page size, rounding to int.
 		search := &Search{
 			Query: searchQuery,
 			NextPage: nextPage,
@@ -106,6 +134,12 @@ func searchHandler(newsapi *news.Client) http.HandlerFunc {
 
 		}
 
+		// increments the page number every time a new page of results is received.
+		if ok := !search.IsLastPage(); ok {
+			search.NextPage++
+		}
+
+		// executing the template, passing the search struct as the data object.
 		buf := &bytes.Buffer{}
 		err = tpl.Execute(buf, search)
 		if err != nil {
--- news/news.go
+++ news/news.go
@@ -23,20 +23,29 @@ type Article struct {
 	Content     string    `json:"content"`
 }
 
+// formats published date from standard time.Time to "January 10, 2009" format.
+func (a *Article) FormatPublishedDate() string {
+	year, month, day := a.PublishedAt.Date()
+	return fmt.Sprintf("%v %d, %d", month, day, year)
+}
+
 type Results struct {
 	Status       string	   `json:"status"`	
 	TotalResults int	   `json:"totalResults"`
 	Articles     []Article `json:"articles"`
 }
 
+// client for working with the News API.
 type Client struct {
 	http     *http.Client
 	key      string
 	PageSize int
 }
 
+// returns results (according to given query and current page) and an error.
 func (c *Client) FetchEverything(query, page string) (*Results, error) {
-	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%s&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(query), c.PageSize, page, c.key)
+	// creating the URL for given query and page number.
+	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%s&apiKey=%s&sortBy=publishedAt", url.QueryEscape(query), c.PageSize, page, c.key)
 	resp, err := c.http.Get(endpoint)
 	if err != nil {
 		return nil, err
@@ -44,19 +53,22 @@ func (c *Client) FetchEverything(query, page string) (*Results, error) {
 
 	defer resp.Body.Close()
 
+	// converting the response body to []byte. 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	if resp.StatusCode != http.StatusOK {
+	if resp.StatusCode != http.StatusOK { //200 OK
 		return nil, fmt.Errorf(string(body))
 	}
 
+	// decode body into the Result.
 	res := &Results{}
 	return res, json.Unmarshal(body, res)
 }
 
+// returns a new Client instance for making requests to rhe NewsAPi.
 func NewClient(httpClient *http.Client, key string, pageSize int) *Client {
 	if pageSize > 100 {
 		pageSize = 100
